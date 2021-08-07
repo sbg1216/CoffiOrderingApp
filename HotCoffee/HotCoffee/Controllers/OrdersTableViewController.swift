0d2f4ae2721a5cbc208921304805b9b31fcf3312
성봉귀 <sbg9004@nate.com>
2021-08-07 19:25:41 +0900

    - 모델, 웹서비스 구현 (todo: 디코딩 에러 해결)

--- HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
+++ HotCoffee/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,5 +10,29 @@ import UIKit
 
 
 class OrdersTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populateOrders()
+    }
     
+    private func populateOrders() {
+        
+        guard let coffeOrdersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com") else {
+            fatalError("URL was incorrect")
+//            return
+        }
+        
+        let resource = Resource<[Order]>(url: coffeOrdersURL)
+        
+        WebService().load(resource: resource) { result in
+            
+            switch result {
+                case .success(let orders):
+                    print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by 성봉귀 on 2021/08/07.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappucino
+    case latte
+    case espresso
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
